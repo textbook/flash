307b728c36adafab3832a03fffc6edef67828ca5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-04-03 12:09:37 +0100

    Configure logging before importing application

--- scripts/launch.py
+++ scripts/launch.py
@@ -2,14 +2,14 @@ import logging
 from os import getenv
 import sys
 
-from flash import app
-
 logging.basicConfig(
     datefmt='%Y/%m/%d %H.%M.%S',
     format='%(levelname)s:%(name)s:%(message)s',
     level=logging.DEBUG,
     stream=sys.stdout,
 )
+
+from flash import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(getenv('PORT', 5000)), debug=False)
