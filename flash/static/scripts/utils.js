7ae1059aca5c0b9c34a1bf7c01881f7901bb7a36
Benjamin Gröhbiel <benjamin.groehbiel@gmail.com>
2016-04-21 09:24:41 +0100

    [#22] Stacks all pivotal tracker tiles (#23)

--- flash/static/scripts/utils.js
+++ flash/static/scripts/utils.js
@@ -57,8 +57,44 @@ var updateServices = function() {
   });
 };
 
+/**
+* This can be wrapped up in a jquery plugin.
+* The current visual feedback is not great and will be improved.
+*/
+var bundleService = function (serviceSelector, interval) {
+  console.log('bundle ', serviceSelector)
+  var active = 0;
+  var stacked = []
+  var wrapper = bundle(serviceSelector);
+
+  function bundle(selector) {
+    var tiles = $(selector).remove();
+    var wrapper = $('<div class="tiles-stack"></div>')
+
+    tiles.each(function (index, obj) {
+      stacked[index] = $(obj);
+      wrapper.append(stacked[index]);
+    });
+    $('.dashboard').append(wrapper);
+
+    return wrapper;
+  }
+
+  function updateStacked() {
+    var currentActive = active++ % stacked.length;
+    wrapper.children().hide();
+
+    stacked[currentActive]
+      .show()
+      .effect('shake', {distance: 10, times: 1});
+  }
+
+  updateStacked();
+  setInterval(updateStacked, interval)
+}
+
 $(document).ready(function () {
+  bundleService('.tracker-tile', 10000);
   updateServices();
   setInterval(updateServices, 60000);
 });
-
